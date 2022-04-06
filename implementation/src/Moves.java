86f496703ee936b2a4c12f8bc73a2d6bd0abca81
pratheek3010 <pratheekraviprakash@gmail.com>
2022-04-06 14:08:32 +0530

    "damage matrix"

--- implementation/src/Moves.java
+++ implementation/src/Moves.java
@@ -2,9 +2,9 @@ class DamageMatrix {
     float[][] damageMatrix; 
     DamageMatrix() {
         damageMatrix = new float[3][3];
-        damageMatrix[0][0] = damageMatrix[1][1] = damageMatrix[2][2] = 1;
-        damageMatrix[0][1] = damageMatrix[1][2] = damageMatrix[2][0] = (float)0.5;
-        damageMatrix[0][2] = damageMatrix[1][0] = damageMatrix[2][1] = 2;
+        damageMatrix[0][0] = damageMatrix[1][1] = damageMatrix[2][2] = (float)0.1;
+        damageMatrix[0][1] = damageMatrix[1][2] = damageMatrix[2][0] = (float)0.05;
+        damageMatrix[0][2] = damageMatrix[1][0] = damageMatrix[2][1] = (float)0.2;
     }
 }
 
